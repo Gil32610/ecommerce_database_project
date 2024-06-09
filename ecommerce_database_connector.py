1154bc6b7f2215e28da1da9b563d51ddb7385cbf
gil.00000032610@unicap.br <gil.00000032610@unicap.br>
2024-06-09 17:03:49 -0300

    cursor

--- ecommerce_database_connector.py
+++ ecommerce_database_connector.py
@@ -8,6 +8,8 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM funcionario")
+mycursor.executemany() #executa uma formula com um conjunto de entradas
+
 
 for entries in mycursor:
     print(entries)
